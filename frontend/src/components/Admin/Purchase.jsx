b8d095849725b340c931d4b886bea1ce5ca143d1
Priyanshu Modi <70794004+priyanshumodi@users.noreply.github.com>
2024-05-07 09:15:04 +0000

    components

--- frontend/src/components/Admin/Purchase.jsx
+++ frontend/src/components/Admin/Purchase.jsx
@@ -2,9 +2,67 @@ import React from "react";
 
 function Purchase() {
     return (
-        <div>
-            Purchase
-        </div>
+        <div class="container mx-auto p-6 dark:bg-zinc-800">
+        <table class="min-w-full bg-white dark:bg-zinc-900">
+          <thead>
+            <tr>
+              <th
+                scope="col"
+                class="px-6 py-3 text-left text-xs font-medium text-zinc-500 dark:text-zinc-400 uppercase">
+                Product Name
+              </th>
+              <th
+                scope="col"
+                class="px-6 py-3 text-left text-xs font-medium text-zinc-500 dark:text-zinc-400 uppercase">
+                Quantity
+              </th>
+              <th
+                scope="col"
+                class="px-6 py-3 text-left text-xs font-medium text-zinc-500 dark:text-zinc-400 uppercase">
+                Pricing
+              </th>
+              <th
+                scope="col"
+                class="px-6 py-3 text-left text-xs font-medium text-zinc-500 dark:text-zinc-400 uppercase">
+                MRP
+              </th>
+              <th
+                scope="col"
+                class="px-6 py-3 text-left text-xs font-medium text-zinc-500 dark:text-zinc-400 uppercase">
+                Purchase order ID
+              </th>
+              <th
+                scope="col"
+                class="px-6 py-3 text-left text-xs font-medium text-zinc-500 dark:text-zinc-400 uppercase">
+                Customer ID
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-zinc-900 dark:text-zinc-200">
+                Jane Smith
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-zinc-900 dark:text-zinc-200">
+                janesmith@example.com
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-zinc-900 dark:text-zinc-200">
+                987-654-3210
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-zinc-900 dark:text-zinc-200">
+                Los Angeles
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-zinc-900 dark:text-zinc-200">
+                CUST123
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-zinc-900 dark:text-zinc-200">
+                CUST124
+              </td>
+            </tr>
+            
+          </tbody>
+        </table>
+          </div>
     )
 }
 
--- frontend/src/components/Admin/Shipping.jsx
+++ frontend/src/components/Admin/Shipping.jsx
@@ -2,9 +2,59 @@ import React from "react";
 
 function Shipping() {
     return (
-        <div>
-            Shipping
-        </div>
+        <div class="container mx-auto p-6 dark:bg-zinc-800">
+        <table class="min-w-full bg-white dark:bg-zinc-900">
+          <thead>
+            <tr>
+              <th
+                scope="col"
+                class="px-6 py-3 text-left text-xs font-medium text-zinc-500 dark:text-zinc-400 uppercase">
+                Address
+              </th>
+              <th
+                scope="col"
+                class="px-6 py-3 text-left text-xs font-medium text-zinc-500 dark:text-zinc-400 uppercase">
+                City
+              </th>
+              <th
+                scope="col"
+                class="px-6 py-3 text-left text-xs font-medium text-zinc-500 dark:text-zinc-400 uppercase">
+                Pincode
+              </th>
+              <th
+                scope="col"
+                class="px-6 py-3 text-left text-xs font-medium text-zinc-500 dark:text-zinc-400 uppercase">
+                Purchase order ID
+              </th>
+              <th
+                scope="col"
+                class="px-6 py-3 text-left text-xs font-medium text-zinc-500 dark:text-zinc-400 uppercase">
+                Customer ID
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-zinc-900 dark:text-zinc-200">
+                Jane Smith
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-zinc-900 dark:text-zinc-200">
+                janesmith@example.com
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-zinc-900 dark:text-zinc-200">
+                987-654-3210
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-zinc-900 dark:text-zinc-200">
+                Los Angeles
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-zinc-900 dark:text-zinc-200">
+                CUST123
+              </td>
+            </tr>
+            
+          </tbody>
+        </table>
+          </div>
     )
 }
 
